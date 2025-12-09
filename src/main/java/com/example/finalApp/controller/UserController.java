0245d14ef55192ce8d742de2091125f6ddf277c1
sucheoli <kingsucheoli@gmail.com>
2025-12-10 00:53:36 +0900

    login, join

--- src/main/java/com/example/finalApp/controller/UserController.java
+++ src/main/java/com/example/finalApp/controller/UserController.java
@@ -1,5 +1,82 @@
 package com.example.finalApp.controller;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.finalApp.dto.UserDTO;
+import com.example.finalApp.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+   private final UserService userService;
+
+   // 요청 URL : GET /user/join => joinForm()메소드 실행
+   @GetMapping("/join")
+   public String joinForm(Model model) {
+      if (!model.containsAttribute("user")) {
+         // Model에 user가 없으면 새 UserDTO(빈 객체) 넣기
+         model.addAttribute("user", new UserDTO());
+      }
+      return "user/join"; // templates/user/join.html
+   }
+
+   // 회원가입 처리
+   // 요청 URL : POST /user/join => join() 메소드 실행
+   @PostMapping("/join")
+   public String join(@ModelAttribute("user") UserDTO user, @RequestParam("confirm-password") String confirmPassword,
+         RedirectAttributes ra) {
+
+      try {
+         userService.join(user, confirmPassword);
+         ra.addFlashAttribute("msg", "회원가입이 완료되었습니다. 로그인해주세요.");
+         return "redirect:/user/login";
+      } catch (Exception e) {
+         ra.addFlashAttribute("error", "회원가입중 오류가 발생했습니다.");
+         return "redirect:/user/join";
+      }
+
+   }
+
+   // 요청 URL : GET /user/login => loginForm() 메소드 실행
+   @GetMapping("/login")
+   public String loginForm() {
+      return "user/login";
+   }
+
+   // 요청 URL : POST /user/login => login() 메소드 실행
+   @PostMapping("/login")
+   public String login(@RequestParam("loginId") String loginId, @RequestParam("password") String password,
+         HttpSession session, RedirectAttributes ra) {
+      Optional<Long> userId = userService.login(loginId, password);
+      if (userId.isPresent()) {
+         session.setAttribute("userId", userId.get());
+         session.setAttribute("loginId", userId);
+         return "redirect:/board/list"; // 로그인 성공 시 게시판으로 이동
+      } else {
+         ra.addFlashAttribute("error", "아이디 또는 비밀번호가 틀렸습니다.");
+         return "redirect:/user/login"; // 로그인 실패 시 다시 로그인 페이지로 이동
+      }
+
+   }
+
+   // 요청 URL : GET /user/logout => logout() 메소드 실행
+   @GetMapping("/logout")
+   public String logout(HttpSession session) {
+      session.invalidate();
+      return "redirect:/user/login";
+   }
 
 }
--- src/main/java/com/example/finalApp/service/UserServiceImpl.java
+++ src/main/java/com/example/finalApp/service/UserServiceImpl.java
@@ -1,5 +1,39 @@
 package com.example.finalApp.service;
 
-public class UserServiceImpl {
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.finalApp.dto.UserDTO;
+import com.example.finalApp.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service // 스프링이 서비스 빈으로 등록(트랜잭션, 비즈니스 로직 위치)
+@RequiredArgsConstructor // final 필드(userRepository)를 매개변수로 받는 생성자 자동 생성 -> 생성자 주입(DI)
+public class UserServiceImpl implements UserService {
+
+   private final UserRepository userRepository; // 실제 DB접근을 수행하는 영속성 계층
+   // private final : 반드시 필요하고, 변경되면 안되는 의존성이라는 의미
+   // final이라 생성자 에서만 초기화가 가능 -> 필드 주입이 아닌 생성자 주입을 강제하는 패턴
+   // NPE(NullPointerException)을 방지, 테스트 코드 작성시 의존성이 명확, 순환 참조를 발견 쉬움
+
+   @Override
+   public void join(UserDTO user, String confirmPassword) {
+      // 비밀번호 확인 체크
+      if (!user.getPassword().equals(confirmPassword)) {
+         throw new IllegalArgumentException("비밀번호와 비밀번호 확인이 일치하지 않습니다");
+      }
+
+      // DB 저장 호출
+      userRepository.insertUser(user);
+
+   }
+
+   @Override
+   public Optional<Long> login(String loginId, String password) {
+      //레포지토리에게 조회 위임
+      return userRepository.findUserNumber(loginId, password);
+   }
 
 }
